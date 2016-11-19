9d02a896238ab8cc95e9703bdab90eef21fe88cd
Mariano Navas <marianudo@gmail.com>
2016-11-19 18:33:15 +0100

    Define grid ATD (Algebraic Data Type)

---
+++ src/main/scala/me/marianonavas/tictactoe/Grid.scala
@@ -0,0 +1,14 @@
+package me.marianonavas.tictactoe
+
+/*
+  * Given that the number of possible positions inside the game grid is limited and small an algebraic data type (ADT)
+  * will be used to represent those possible positions. This way we enforce compile time checking of the game grid
+  * positions and avoid the need of parameter validations and their testing.
+  */
+sealed trait GridCoordinate
+
+object First extends GridCoordinate
+
+object Center extends GridCoordinate
+
+object Last extends GridCoordinate
