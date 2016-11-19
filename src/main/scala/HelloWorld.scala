5758540743c56cafe284c06764d1b40080763922
Mariano Navas <marianudo@gmail.com>
2016-11-19 18:16:34 +0100

    Project creation

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+package me.marianonavas.tictactoeserver
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello World!!!")
+  }
+}
+
---
+++ src/test/scala/HelloWorldSpec.scala
@@ -0,0 +1,12 @@
+package me.marianonavas.tictactoeserver
+
+import org.scalatest.FunSpec
+
+class HelloWorldSpec extends FunSpec {
+  describe("Adding 1 to 1") {
+    it("should equals 2"){
+      assert(1+1 == 2)
+    }
+  }
+}
+
